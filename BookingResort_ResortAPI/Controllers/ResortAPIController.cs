bd8bbe88558e6d3b033cee1dc4bed0b84839d23f
Sayan Khutia <sayank.khutia123@gmail.com>
2023-03-27 22:05:56 +0530

    First API Endpoint - HTTPGET

--- BookingResort_ResortAPI/Controllers/ResortAPIController.cs
+++ BookingResort_ResortAPI/Controllers/ResortAPIController.cs
@@ -1,10 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookingResort_ResortAPI.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BookingResort_ResortAPI.Controllers
 {
+	[Route("api/ResortAPI")]
 	[ApiController]
 	public class ResortAPIController : ControllerBase
 	{
-
+		[HttpGet]
+		public IEnumerable<Resort> GetResorts()
+		{ 
+			return new List<Resort> { 
+				new Resort{ Id = 1, Name = "Pool View"},
+				new Resort{ Id = 2, Name = "Beach View"}
+			};
+		}
 	}
 }
---
+++ BookingResort_ResortAPI/Models/Resort.cs
@@ -0,0 +1,8 @@
+﻿namespace BookingResort_ResortAPI.Models
+{
+	public class Resort
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+	}
+}
