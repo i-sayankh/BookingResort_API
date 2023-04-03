53b332874f55bee1680651d60529a86649297748
Sayan Khutia <sayank.khutia123@gmail.com>
2023-04-03 19:28:56 +0530

    ModelState Validation

--- BookingResort_ResortAPI/Models/DTO/ResortDTO.cs
+++ BookingResort_ResortAPI/Models/DTO/ResortDTO.cs
@@ -1,8 +1,12 @@
-﻿namespace BookingResort_ResortAPI.Models.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookingResort_ResortAPI.Models.DTO
 {
 	public class ResortDTO
 	{
 		public int Id { get; set; }
+		[Required]
+		[MaxLength(30)]
 		public string Name { get; set; }
 	}
 }
