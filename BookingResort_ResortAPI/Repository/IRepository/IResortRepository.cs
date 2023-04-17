2b2a2e474c68cc6f44ccf16cd744c130390acd73
Sayan Khutia <sayank.khutia123@gmail.com>
2023-04-17 12:53:58 +0530

    Add Resort Repository Interface

---
+++ BookingResort_ResortAPI/Repository/IRepository/IResortRepository.cs
@@ -0,0 +1,14 @@
+﻿using BookingResort_ResortAPI.Models;
+using System.Linq.Expressions;
+
+namespace BookingResort_ResortAPI.Repository.IRepository
+{
+	public interface IResortRepository
+	{
+		Task<List<Resort>> GetAll(Expression<Func<Resort>> filter = null);
+		Task<Resort> Get(Expression<Func<Resort>> filter = null, bool tracked=true);
+		Task Create(Resort entity);
+		Task Remove(Resort entity);
+		Task Save();
+	}
+}
