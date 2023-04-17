3580d4cde7fb57bee618893ee8863aeb81366eab
Sayan Khutia <sayank.khutia123@gmail.com>
2023-04-17 12:12:59 +0530

    Set up Auto Mapper and MappingConfig

---
+++ BookingResort_ResortAPI/MappingConfig.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using BookingResort_ResortAPI.Models;
+using BookingResort_ResortAPI.Models.DTO;
+
+namespace BookingResort_ResortAPI
+{
+	public class MappingConfig : Profile
+	{
+		public MappingConfig()
+		{
+			CreateMap<Resort, ResortDTO>().ReverseMap();
+			CreateMap<Resort, ResortCreateDTO>().ReverseMap();
+			CreateMap<Resort, ResortUpdateDTO>().ReverseMap();
+		}
+	}
+}
--- BookingResort_ResortAPI/Program.cs
+++ BookingResort_ResortAPI/Program.cs
@@ -1,3 +1,4 @@
+using BookingResort_ResortAPI;
 using BookingResort_ResortAPI.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,6 +9,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(option =>
 {
 	option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultSQLConnection"));
 });
+builder.Services.AddAutoMapper(typeof(MappingConfig));
 builder.Services.AddControllers(option => {
 	//option.ReturnHttpNotAcceptable=true;
 }).AddNewtonsoftJson().AddXmlDataContractSerializerFormatters();
