4b91c8ef1cd9543bee963de61d1e8375ab6210c9
Sayan Khutia <sayank.khutia123@gmail.com>
2023-04-18 12:33:31 +0530

    Repository CleanUp

--- BookingResort_ResortAPI/Repository/IRepository/IResortRepository.cs
+++ BookingResort_ResortAPI/Repository/IRepository/IResortRepository.cs
@@ -3,13 +3,8 @@ using System.Linq.Expressions;
 
 namespace BookingResort_ResortAPI.Repository.IRepository
 {
-	public interface IResortRepository
-	{
-		Task<List<Resort>> GetAllAsync(Expression<Func<Resort, bool>> filter = null);
-		Task<Resort> GetAsync(Expression<Func<Resort, bool>> filter = null, bool tracked=true);
-		Task CreateAsync(Resort entity);
-		Task UpdateAsync(Resort entity);
-		Task RemoveAsync(Resort entity);
-		Task SaveAsync();
+	public interface IResortRepository : IRepository<Resort>
+	{		
+		Task<Resort> UpdateAsync(Resort entity);		
 	}
 }
--- BookingResort_ResortAPI/Repository/ResortRepository.cs
+++ BookingResort_ResortAPI/Repository/ResortRepository.cs
@@ -7,58 +7,20 @@ using System.Linq.Expressions;
 
 namespace BookingResort_ResortAPI.Repository
 {
-	public class ResortRepository : IResortRepository
+	public class ResortRepository : Repository<Resort>, IResortRepository
 	{
 		private readonly ApplicationDbContext _db;
-		public ResortRepository(ApplicationDbContext db)
+		public ResortRepository(ApplicationDbContext db) : base(db) 
 		{
 			_db = db;
-		}
-		public async Task CreateAsync(Resort entity)
-		{
-			await _db.Resorts.AddAsync(entity);
-			await SaveAsync();
-		}
+		}	
 
-		public async Task<Resort> GetAsync(Expression<Func<Resort, bool>> filter = null, bool tracked = true)
+		public async Task<Resort> UpdateAsync(Resort entity)
 		{
-			IQueryable<Resort> query = _db.Resorts;
-			if(!tracked)
-			{
-				query = query.AsNoTracking();
-			}
-			if (filter != null)
-			{
-				query = query.Where(filter);
-			}
-			return await query.FirstOrDefaultAsync();
-		}
-
-		public async Task<List<Resort>> GetAllAsync(Expression<Func<Resort, bool>> filter = null)
-		{
-			IQueryable<Resort> query = _db.Resorts;
-			if(filter != null)
-			{
-				query = query.Where(filter);
-			}
-			return await query.ToListAsync();
-		}
-
-		public async Task RemoveAsync(Resort entity)
-		{
-			_db.Resorts.Remove(entity);
-			await SaveAsync();
-		}
-
-		public async Task SaveAsync()
-		{
-			await _db.SaveChangesAsync();
-		}
-
-		public async Task UpdateAsync(Resort entity)
-		{
+			entity.UpdatedDate = DateTime.Now;
 			_db.Resorts.Update(entity);
-			await SaveAsync();
+			await _db.SaveChangesAsync();
+			return entity;
 		}
 	}
 }
