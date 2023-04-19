98f4359a0ab2ac01df8f53e7fe2c03a21eefbec5
Sayan Khutia <sayank.khutia123@gmail.com>
2023-04-19 16:27:39 +0530

    Base Service for API

---
+++ BookingResort_Web/Services/IServices/IBaseService.cs
@@ -0,0 +1,10 @@
+﻿using BookingResort_Web.Models;
+
+namespace BookingResort_Web.Services.IServices
+{
+	public interface IBaseService
+	{
+		APIResponse responseModel { get; set; }
+		Task<T> SendAsync<T>(APIRequest apiRequest);
+	}
+}
