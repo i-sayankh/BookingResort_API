7a1f6dc4e2ea6bd42f8551412f74b7b5d712c167
Sayan Khutia <sayank.khutia123@gmail.com>
2023-04-19 14:16:21 +0530

    Web Project DTO's and API Models

---
+++ BookingResort_Web/Models/APIRequest.cs
@@ -0,0 +1,11 @@
+﻿using static BookingResort_Utility.SD;
+
+namespace BookingResort_Web.Models
+{
+	public class APIRequest
+	{
+		public ApiType ApiType { get; set; } = ApiType.GET; 
+		public string Url { get; set; }
+		public Object Data { get; set; }
+	}
+}
---
+++ BookingResort_Web/Models/DTO/ResortNumberUpdateDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookingResort_Web.Models.DTO
+{
+	public class ResortNumberUpdateDTO
+	{
+		[Required]
+		public int ResortNo { get; set; }
+		[Required]
+		public int ResortId { get; set; }
+		public string SpecialDetails { get; set; }
+	}
+}
