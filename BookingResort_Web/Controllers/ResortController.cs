9a7b61951541ad3df7690522629ed6854246aea4
Sayan Khutia <sayank.khutia123@gmail.com>
2023-04-24 21:09:00 +0530

    Delete Resort

--- BookingResort_Web/Controllers/ResortController.cs
+++ BookingResort_Web/Controllers/ResortController.cs
@@ -76,5 +76,28 @@ namespace BookingResort_Web.Controllers
             }
             return View(model);
         }
+
+        public async Task<IActionResult> DeleteResort(int resortId)
+        {
+            var response = await _resortService.GetAsync<APIResponse>(resortId);
+            if (response != null && response.IsSuccess)
+            {
+                ResortDTO model = JsonConvert.DeserializeObject<ResortDTO>(Convert.ToString(response.Result));
+                return View(model);
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteResort(ResortDTO model)
+        {
+            var response = await _resortService.DeleteAsync<APIResponse>(model.Id);
+            if (response != null && response.IsSuccess)
+            {
+                return RedirectToAction(nameof(IndexResort));
+            }
+            return View(model);
+        }
     }
 }
