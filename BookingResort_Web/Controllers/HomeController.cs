6f1c7df92cab4ba21b928185a84077681e579938
Sayan Khutia <sayank.khutia123@gmail.com>
2023-04-28 13:03:14 +0530

    Resort Home Page

--- BookingResort_Web/Controllers/HomeController.cs
+++ BookingResort_Web/Controllers/HomeController.cs
@@ -1,32 +1,33 @@
-﻿using BookingResort_Web.Models;
+﻿using AutoMapper;
+using BookingResort_Web.Models;
+using BookingResort_Web.Models.DTO;
+using BookingResort_Web.Services.IServices;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System.Diagnostics;
 
 namespace BookingResort_Web.Controllers
 {
 	public class HomeController : Controller
 	{
-		private readonly ILogger<HomeController> _logger;
+        private readonly IResortService _resortService;
+        private readonly IMapper _mapper;
 
-		public HomeController(ILogger<HomeController> logger)
-		{
-			_logger = logger;
-		}
+        public HomeController(IResortService resortService, IMapper mapper)
+        {
+            _resortService = resortService;
+            _mapper = mapper;
+        }
 
-		public IActionResult Index()
-		{
-			return View();
-		}
-
-		public IActionResult Privacy()
-		{
-			return View();
-		}
-
-		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-		public IActionResult Error()
-		{
-			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-		}
+        public async Task<IActionResult> Index()
+        {
+            List<ResortDTO> list = new();
+            var response = await _resortService.GetAllAsync<APIResponse>();
+            if (response != null && response.IsSuccess)
+            {
+                list = JsonConvert.DeserializeObject<List<ResortDTO>>(Convert.ToString(response.Result));
+            }
+            return View(list);
+        }
 	}
 }
