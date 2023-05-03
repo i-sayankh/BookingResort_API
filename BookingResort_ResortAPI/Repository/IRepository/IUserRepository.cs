4d79a3bce3a2621aa153b05071a6ab943a1787ee
Sayan Khutia <sayank.khutia123@gmail.com>
2023-05-03 12:18:48 +0530

    Add User Repository

---
+++ BookingResort_ResortAPI/Repository/IRepository/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using BookingResort_ResortAPI.Models;
+using BookingResort_ResortAPI.Models.DTO;
+
+namespace BookingResort_ResortAPI.Repository.IRepository
+{
+    public interface IUserRepository
+    {
+        bool IsUniqueUser(string username);
+        Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO);
+        Task<LocalUser> Register(RegistrationRequestDTO registrationRequestDTO);
+    }
+}
---
+++ BookingResort_ResortAPI/Repository/UserRepository.cs
@@ -0,0 +1,30 @@
+﻿using BookingResort_ResortAPI.Data;
+using BookingResort_ResortAPI.Models;
+using BookingResort_ResortAPI.Models.DTO;
+using BookingResort_ResortAPI.Repository.IRepository;
+
+namespace BookingResort_ResortAPI.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public UserRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public bool IsUniqueUser(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LocalUser> Register(RegistrationRequestDTO registrationRequestDTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
