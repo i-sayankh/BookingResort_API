132e3e03c6e8b1ea5835d19e5bf837f8845a2160
Sayan Khutia <sayank.khutia123@gmail.com>
2023-05-03 12:31:32 +0530

    Implement User Repository - Register

--- BookingResort_ResortAPI/Repository/UserRepository.cs
+++ BookingResort_ResortAPI/Repository/UserRepository.cs
@@ -14,17 +14,32 @@ namespace BookingResort_ResortAPI.Repository
         }
         public bool IsUniqueUser(string username)
         {
-            throw new NotImplementedException();
+            var user = _db.LocalUsers.FirstOrDefault(x => x.UserName== username);
+            if (user == null)
+            {
+                return true;
+            }
+            return false;
         }
 
-        public Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO)
+        public async Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO)
         {
             throw new NotImplementedException();
         }
 
-        public Task<LocalUser> Register(RegistrationRequestDTO registrationRequestDTO)
+        public async Task<LocalUser> Register(RegistrationRequestDTO registrationRequestDTO)
         {
-            throw new NotImplementedException();
+            LocalUser user = new LocalUser
+            {
+                UserName = registrationRequestDTO.Username,
+                Password = registrationRequestDTO.Password,
+                Name = registrationRequestDTO.Name,
+                Role = registrationRequestDTO.Role
+            };
+            _db.LocalUsers.Add(user);
+            await _db.SaveChangesAsync();
+            user.Password = "";
+            return user;
         }
     }
 }
