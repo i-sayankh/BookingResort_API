771413220af017617ee58eef5d52d57e803d0d57
Sayan Khutia <sayank.khutia123@gmail.com>
2023-05-11 12:51:24 +0530

    Consuming Secured API - Auth Controller Action Methods

---
+++ BookingResort_Web/Controllers/AuthController.cs
@@ -0,0 +1,56 @@
+﻿using BookingResort_Web.Models;
+using BookingResort_Web.Models.DTO;
+using BookingResort_Web.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookingResort_Web.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly IAuthService _authService;
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+        [HttpGet]
+        public IActionResult Login()
+        {
+            LoginRequestDTO obj = new();
+            return View(obj);
+        }
+
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public async Task<IActionResult> Login(LoginRequestDTO obj)
+        {
+            return View(obj);
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegistrationRequestDTO obj)
+        {
+            APIResponse result = await _authService.RegisterAsync<APIResponse>(obj);
+            if(result != null && result.IsSuccess)
+            {
+                return RedirectToAction("Login");
+            }
+            return View();
+        }
+        public async Task<IActionResult> Logout()
+        {
+            return View();
+        }
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
+
+    }
+}
--- BookingResort_Web/Services/IServices/IAuthService.cs
+++ BookingResort_Web/Services/IServices/IAuthService.cs
@@ -5,6 +5,6 @@ namespace BookingResort_Web.Services.IServices
     public interface IAuthService
     {
         Task<T> LoginAsync<T>(LoginRequestDTO objToCreate);
-        Task<T> RegisterAsync<T>(UserDTO objToCreate);
+        Task<T> RegisterAsync<T>(RegistrationRequestDTO objToCreate);
     }
 }
